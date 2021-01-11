81ed92d25dd27a6f66326dd47e76e2d9be88189b
CindoddCindy <cindodcindy@gmail.com>
2021-01-11 19:18:07 +0700

    tambah layout

---
+++ app/src/main/java/com/myproject/leaguelist/view/BottomNav.java
@@ -0,0 +1,16 @@
+package com.myproject.leaguelist.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.myproject.leaguelist.R;
+
+public class BottomNav extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_nav);
+    }
+}
---
+++ app/src/main/java/com/myproject/leaguelist/view/Detail.java
@@ -0,0 +1,16 @@
+package com.myproject.leaguelist.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.myproject.leaguelist.R;
+
+public class Detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
